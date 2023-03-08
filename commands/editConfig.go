8b1cc8bb88aeadb968b2ffd4856b7756bd5e16c3
Kevin Verschaeve <minou@pop-os.localdomain>
2023-03-08 16:06:44 +0100

    create config file if it does not exists

--- commands/editConfig.go
+++ commands/editConfig.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"log"
 	"mycli/app"
+	"os"
 
 	"github.com/symfony-cli/console"
 )
@@ -11,6 +13,21 @@ var ConfigEdit = &console.Command{
 	Name:    "config:edit",
 	Aliases: []*console.Alias{{Name: "config"}},
 	Usage:   "Show or edit configuration",
+	Before: func(c *console.Context) error {
+		if _, err := os.Stat(app.MyCliHome() + "/" + app.CONFIG_FILE); os.IsNotExist(err) {
+			if err := os.MkdirAll(app.MyCliHome(), os.ModePerm); err != nil {
+				log.Fatal("Unable to create home directory. Try to create it manually")
+			}
+
+			d1 := []byte("{\n\n}")
+			err := os.WriteFile(app.MyCliHome()+"/"+app.CONFIG_FILE, d1, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		return nil
+	},
 	Action: func(c *console.Context) error {
 		app.OpenCommand(app.MyCliHome() + "/" + app.CONFIG_FILE)
 
